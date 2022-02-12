0b9ff39d57c278bce435f52621befa06a70aac59
jeongminsu <ansuz0616@naver.com>
2022-02-12 16:01:15 +0900

    장바구니 기능 추가

--- spa_mall/routes/goods.js
+++ spa_mall/routes/goods.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Goods = require('../schemas/goods');
+const Cart = require('../schemas/cart');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -25,6 +26,46 @@ router.get('/goods/:goodsId', async (req, res) => {
     detail,
   });
 });
+
+router.post('/goods/:goodsId/cart', async (req, res) => {
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  const existscarts = await Cart.find({ goodsId: Number(goodsId) });
+  if (existscarts.length) {
+    return res.status(400).json({ success: false, errorMessage: '이미 장바구니에 들어있는 상품입니다.' });
+  }
+
+  await Cart.create({ goodsId: Number(goodsId), quantity });
+
+  res.json({ seccess: true });
+
+});
+
+router.delete('/goods/:goodsId/cart', async (req, res) => {
+  const { goodsId } = req.params;
+
+  const existscarts = await Cart.find({ goodsId: Number(goodsId) });
+  if (existscarts.length) {
+    await Cart.deleteOne({ goodsId: Number(goodsId) });
+  }
+
+  res.json({ success: true })
+});
+
+router.put('/goods/:goodsId/cart', async (req, res) => {
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  const existscarts = await Cart.find({ goodsId: Number(goodsId) });
+  if (!existscarts.length) {
+    return res.status(400).json({ success: false, errorMessage: '장바구니에 해당 상품이 없습니다.' });
+  }
+
+  await Cart.updateOne({ goodsId: Number(goodsId) }, { $set: { quantity } })
+
+  res.json({success: true})
+})
 
 router.post('/goods', async (req, res) => {
   const { goodsId, name, thumbnailUrl, category, price } = req.body;
